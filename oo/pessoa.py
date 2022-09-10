1d7f86f02ce72a27e6591b2add18d501712e47e0
Israel <israelisaque@hotmail.com>
2022-09-10 15:34:08 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,17 @@
 class Pessoa:
-    def cumprimenta(self):
-        return 'Olá'
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimenta(p))
+    p = Pessoa('Israel')
+    print(Pessoa.cumprimentar(p))
     print(id(p))
-    print(p.cumprimenta())
+    print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
 
