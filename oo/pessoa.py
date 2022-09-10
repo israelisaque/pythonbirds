386d7e583f9af83fb59347dc58061c686d896f68
Israel <israelisaque@hotmail.com>
2022-09-10 15:54:12 -0300

    Criado atributo completo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Israel')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    israel = Pessoa(nome='Israel')
+    isaque = Pessoa(israel, nome="Isaque")
+    print(Pessoa.cumprimentar(israel))
+    print(id(israel))
+    print(israel.cumprimentar())
+    print(israel.nome)
+    print(israel.idade)
+    for filho in isaque.filhos:
+        print(filho.nome)
 
